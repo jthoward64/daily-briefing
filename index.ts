c1b5bf547c58276b23c60d7bc98f581424c0f6e4
Tag Howard <tag.howard1080@gmail.com>
2022-03-14 18:53:27 -0400

    Add News API

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import { assembleMenuSentence } from "./CampusDishManager.js";
+import NewsAPI from "./NewsApiManager.js";
+import secrets from "./secrets.js";
 
 const username = "Tag";
 
@@ -6,3 +8,13 @@ console.log(`Good morning ${username}.`);
 
 const menuSentence = await assembleMenuSentence();
 console.log(menuSentence);
+
+console.log("In actual news, the current top five stories are:");
+const newsApi = new NewsAPI(secrets.newsApiKey);
+const top = await newsApi.getTopHeadlines({
+  sources: ["associated-press"],
+  pageSize: 5,
+});
+top.articles.forEach((article) =>
+  console.log(`${article.title.replace(/\./g, "")}.`)
+);
