54f13284f0c0a9e66800c3561a03c40c614f02f6
Tag Howard <tag.howard1080@gmail.com>
2022-03-14 18:12:38 -0400

    Move code into assembleMenuSentence

--- CampusDishManager.ts
+++ CampusDishManager.ts
@@ -36,10 +36,11 @@ export async function getCampusDish(
       apiPeriodId = "1068";
       break;
     case "Brunch":
-      apiPeriodId = "1069";
+      apiPeriodId =
+        queryOptions.location === "Champions Kitchen" ? "11090" : "1070";
       break;
     case "Dinner":
-      apiPeriodId = "1070";
+      apiPeriodId = "1069";
       break;
     default:
       console.error("Unknown meal period provided");
@@ -128,4 +129,66 @@ export function isProductImportant(menuProduct: MenuProduct): boolean {
   }
 
   return !hasUnimportantCategory;
+}
+
+export async function assembleMenuSentence() {
+  // Output the day's menu
+  let menuSentence = "";
+
+  // TODO use this api endpoint instead https://uky.campusdish.com/api/menu/GetMenuPeriods?locationId=11090&storeId=&date=03/21/2022&mode=Daily
+  let todayMeals = ["Breakfast", "Lunch", "Dinner"] as (
+    | "Breakfast"
+    | "Lunch"
+    | "Dinner"
+    | "Brunch"
+  )[];
+  const today = new Date("3/20/22");
+  if (today.getDay() === 0 || today.getDay() === 6) {
+    todayMeals = ["Brunch", "Dinner"] as (
+      | "Breakfast"
+      | "Lunch"
+      | "Dinner"
+      | "Brunch"
+    )[];
+  }
+
+  for (const mealPeriod of todayMeals) {
+    menuSentence += `\nToday's ${mealPeriod} menu:\n`;
+
+    const productsAtStations = getProducts(
+      await getCampusDish({
+        date: today,
+        location: "Fresh Food Company",
+        mealPeriod,
+        mode: "Daily",
+      }),
+      ["13878", "13880", "39895", "38109", "24552", "24555", "24542"]
+    );
+
+    for (const stationName in productsAtStations) {
+      if (
+        Object.prototype.hasOwnProperty.call(productsAtStations, stationName)
+      ) {
+        const stationProducts = productsAtStations[stationName];
+        const productNames = stationProducts
+          .filter(isProductImportant)
+          .map((product) => product.Product.MarketingName);
+
+        menuSentence += `${stationName} is serving `;
+        productNames.forEach((productName, i) => {
+          if (i > 0) {
+            if (i === productNames.length - 1) {
+              menuSentence += ", and ";
+            } else {
+              menuSentence += ", ";
+            }
+          }
+          menuSentence += productName.toLowerCase();
+        });
+        menuSentence += ".\n";
+      }
+    }
+  }
+
+  return menuSentence;
 }
--- index.ts
+++ index.ts
@@ -1,67 +1,8 @@
-import {
-  getCampusDish,
-  getProducts,
-  isProductImportant,
-} from "./CampusDishMenu.js";
+import { assembleMenuSentence } from "./CampusDishManager.js";
 
 const username = "Tag";
 
-console.log(`Good morning ${username}`);
+console.log(`Good morning ${username}.`);
 
-// Output the day's menu
-let menuSentence = "";
-
-// TODO use this api endpoint instead https://uky.campusdish.com/api/menu/GetMenuPeriods?locationId=11090&storeId=&date=03/21/2022&mode=Daily
-let todayMeals = ["Breakfast", "Lunch", "Dinner"] as (
-  | "Breakfast"
-  | "Lunch"
-  | "Dinner"
-  | "Brunch"
-)[];
-const today = new Date("3/21/22");
-if (today.getDay() === 0 || today.getDay() === 6) {
-  todayMeals = ["Brunch", "Dinner"] as (
-    | "Breakfast"
-    | "Lunch"
-    | "Dinner"
-    | "Brunch"
-  )[];
-}
-
-for (const mealPeriod of todayMeals) {
-  menuSentence += `Today's ${mealPeriod} menu:\n`;
-
-  const productsAtStations = getProducts(
-    await getCampusDish({
-      date: today,
-      location: "Fresh Food Company",
-      mealPeriod,
-      mode: "Daily",
-    }),
-    []
-  );
-
-  for (const stationName in productsAtStations) {
-    if (Object.prototype.hasOwnProperty.call(productsAtStations, stationName)) {
-      const stationProducts = productsAtStations[stationName];
-      const productNames = stationProducts
-        .filter(isProductImportant)
-        .map((product) => product.Product.MarketingName);
-
-      menuSentence += `${stationName} is serving `;
-      productNames.forEach((productName, i) => {
-        if (i > 0) {
-          if (i === productNames.length - 1) {
-            menuSentence += ", and ";
-          } else {
-            menuSentence += ", ";
-          }
-        }
-        menuSentence += productName.toLowerCase();
-      });
-      menuSentence += ".\n";
-    }
-  }
-}
-
+const menuSentence = await assembleMenuSentence();
 console.log(menuSentence);
