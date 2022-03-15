038c92cc9b3481dad11e93df06154bb2549a414e
Tag Howard <tag.howard1080@gmail.com>
2022-03-15 14:39:23 -0400

    Convert to an AWS lambda function

--- index.ts
+++ index.ts
@@ -3,18 +3,28 @@ import NewsAPI from "./NewsApiManager.js";
 import secrets from "./secrets.js";
 
 const username = "Tag";
-
-console.log(`Good morning ${username}.`);
-
 const menuSentence = await assembleMenuSentence();
-console.log(menuSentence);
-
-console.log("In actual news, the current top five stories are:");
 const newsApi = new NewsAPI(secrets.newsApiKey);
 const top = await newsApi.getTopHeadlines({
   sources: ["associated-press"],
   pageSize: 5,
 });
-top.articles.forEach((article) =>
-  console.log(`${article.title.replace(/\./g, "")}.`)
-);
+
+export async function handler() {
+  let returnValue = "";
+  returnValue += `Good morning ${username}.`;
+
+  returnValue += menuSentence;
+
+  returnValue += "In actual news, the current top five stories are:";
+  top.articles.forEach(
+    (article) => (returnValue += `${article.title.replace(/\./g, "")}.`)
+  );
+
+  const response = {
+    statusCode: 200,
+    body: returnValue,
+  };
+  console.log(response);
+  return response;
+}
